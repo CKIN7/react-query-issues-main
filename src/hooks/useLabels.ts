64e263ab8e3bc14af46f8bb4fa8e38d932b17555
CKIN7 <covain77@gmail.com>
2024-04-09 16:34:07 -0400

    Loading icon, helper, y Placeholder initialData

--- src/hooks/useLabels.ts
+++ src/hooks/useLabels.ts
@@ -1,8 +1,10 @@
 import { useQuery } from "@tanstack/react-query"
 import { Label } from "../interfaces/label"
 import { gitHubApi } from "../../api/gitHubApi"
+import { sleep } from "../helpers/sleep"
 
 const getLabels = async ():Promise<Label[]> => {
+    await sleep(2)
     const { data } = await gitHubApi.get<Label[]>('/labels')
     return data
 }
@@ -10,10 +12,31 @@ const getLabels = async ():Promise<Label[]> => {
 
 export const useLabels = () => {
   
-    labelsQuery = useQuery({
+    const labelsQuery = useQuery({
         queryKey: ['labels'],
         queryFn: getLabels,
-        staleTime: 1000 * 60 * 60,
-    })
+        refetchOnWindowFocus: false,
+        // staleTime: 1000 * 60 * 60,
+        // initialData: [], // initial data funciona con el stale time, reactquery confia que esa es la ultima data (que esta fresh) en el cache
+        // placeholderData: [
+        //   {
+        //     id: 69105383,
+        //     node_id: "MDU6TGFiZWw2OTEwNTM4Mw==",
+        //     url: "https://api.github.com/repos/facebook/react/labels/Browser:%20IE",
+        //     name: "Browser: IE",
+        //     color: "c7def8",
+        //     default: false,
+        // },
+        // {
+        //     id: 69105358,
+        //     node_id: "MDU6TGFiZWw2OTEwNTM1OA==",
+        //     url: "https://api.github.com/repos/facebook/react/labels/Browser:%20Safari",
+        //     name: "Browser: Safari",
+        //     color: "c7def8",
+        //     default: false,
+        // }
+        // ] // placeholderData, mientras se hace la peticion muestra esta data y a pesar de que tenemos el staleTime despues de que se resuelve el fetching ya ahi tenemos eso y no muestra el loading porque tenemos data
+      })
 
+    return labelsQuery  
 }
--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,13 +1,25 @@
+import Oval from "react-loading-icons/dist/esm/components/oval"
+import { useLabels } from "../../hooks/useLabels"
+
 
 export const LabelPicker = () => {
+
+  const labelsQuery = useLabels()
+
+  if(labelsQuery.isLoading) return <Oval />
   return (
     <div>
-        <span 
+       {
+        labelsQuery.data?.map(label => (
+          <span 
+            key={label.id}
             className="badge rounded-pill m-1 label-picker"
-            style={{ border: `1px solid #ffccd3`, color: '#ffccd3' }}
-        >
-            Primary
-        </span>
+            style={{ border: `1px solid ${label.color}`, color: label.color }}
+          >
+            {label.name}
+          </span>
+        ))
+       }
         
     </div>
   )
