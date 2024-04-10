e21247e99478eed3719305b62eebc943019e0a5f
CKIN7 <covain77@gmail.com>
2024-04-10 09:20:26 -0400

    Filtrar Issues Open y Closed

--- src/hooks/useIssues.ts
+++ src/hooks/useIssues.ts
@@ -1,19 +1,30 @@
 import { useQuery } from "@tanstack/react-query"
 import { gitHubApi } from "../../api/gitHubApi"
-import { Issue } from "../interfaces/"
+import { Issue, State } from "../interfaces/"
 import { sleep } from "../helpers/sleep"
 
-const getIssues = async(): Promise<Issue[]> => {
+interface Props {
+    state?: State;
+    labels: string[];
+}
+
+const getIssues = async(labels: string[] = [], state?: State): Promise<Issue[]> => {
     await sleep(2)
-    const { data } = await gitHubApi.get<Issue[]>('issues')
+
+    const params = new URLSearchParams()
+
+    if ( state ) params.append('state', state);
+
+    const { data } = await gitHubApi.get<Issue[]>('issues', { params })
+
     return data
 }
 
-export const useIssues = () => {
+export const useIssues = ({ state, labels }: Props) => {
 
     const issuesQuery = useQuery({
-        queryKey: ['issues'],
-        queryFn: getIssues,
+        queryKey: ['issues', { state, labels }],
+        queryFn: () => getIssues(labels, state),
         // refetchInterval: 5000
     })
 
