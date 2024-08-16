6fd7275ef1e2221fd4d0fc5d656ca4b065145242
CKIN7 <covain77@gmail.com>
2024-08-16 10:01:31 -0400

    sleep

--- src/hooks/useIssue.ts
+++ src/hooks/useIssue.ts
@@ -1,36 +1,35 @@
-import { useQuery } from "@tanstack/react-query"
-import { gitHubApi } from "../../api/gitHubApi"
-import { sleep } from "../helpers/sleep"
-import { Issue } from "../interfaces"
+import { useQuery } from '@tanstack/react-query';
+import { gitHubApi } from '../../api/gitHubApi';
+import { Issue } from '../interfaces';
 
+export const getIssueInfo = async (issueNumber: number): Promise<Issue> => {
+    const { data } = await gitHubApi.get<Issue>(`/issues/${issueNumber}`);
+    return data;
+};
 
-export const getIssueInfo = async( issueNumber: number): Promise<Issue> => {
-    await sleep(2)
-    const { data } = await gitHubApi.get<Issue>(`/issues/${ issueNumber }`)
-    return data
-}
-
-export const getIssueComments = async( issueNumber: number): Promise<Issue[]> => {
-    await sleep(2)
-    const { data } = await gitHubApi.get<Issue[]>(`/issues/${ issueNumber }/comments`)
-    return data
-}
+export const getIssueComments = async (
+    issueNumber: number
+): Promise<Issue[]> => {
+    const { data } = await gitHubApi.get<Issue[]>(
+        `/issues/${issueNumber}/comments`
+    );
+    return data;
+};
 
 export const useIssue = (issueNumber: number) => {
- 
     const issueQuery = useQuery({
         queryKey: ['issue', issueNumber],
         queryFn: () => getIssueInfo(issueNumber),
-    })
+    });
 
     const commentsQuery = useQuery({
         queryKey: ['issue', issueNumber, 'comments'],
         queryFn: () => getIssueComments(issueQuery.data!.number),
-        enabled: issueQuery.data !== undefined
-    })
+        enabled: issueQuery.data !== undefined,
+    });
 
     return {
         issueQuery,
         commentsQuery,
-    }
-}
+    };
+};
