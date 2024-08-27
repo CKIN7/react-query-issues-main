db7449c969ee0d0de65d2793bd70e0c7929faa76
CKIN7 <covain77@gmail.com>
2024-08-27 13:50:54 -0400

    Test Infinite Scroll

--- src/GitApp.tsx
+++ src/GitApp.tsx
@@ -1,13 +1,25 @@
 import { FC } from 'react';
 import { Outlet } from 'react-router';
 
-export const GitApp: FC = () => {
-  
-  return (
-    <div className="container mt-3">
-    <h1>Git Issues <small>Seguimiento de problemas</small> </h1>
-      <Outlet />
-    </div>
-  )
-}
+import { Link } from 'react-router-dom';
 
+export const GitApp: FC = () => {
+    return (
+        <div className="container mt-3">
+            <h1>
+                <Link to="/issues/list">Git Issues</Link>
+                <small>Seguimiento de problemas</small>
+            </h1>
+            <nav>
+                <ul style={{ listStyleType: 'none' }}>
+                    <li style={{ fontSize: '22px' }}>
+                        <Link to="/issues/list/infinite">
+                            Test Infinite Scroll
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+            <Outlet />
+        </div>
+    );
+};
